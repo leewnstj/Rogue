1cb9f84602bacdb7c92ebdb2bb19cbe285283e25
leewnstj <ij35492581@gmail.com>
2023-01-30 23:49:38 +0900

    jump
    jump

--- Assets/02_Scripts/AgentMovement.cs
+++ Assets/02_Scripts/AgentMovement.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class AgentMovement : MonoBehaviour
 {
+    [Header("플레이어 스탯")]
     [SerializeField] float PlayerSpeed;
     [SerializeField] float PlayerJumpPower;
+    [SerializeField] float DashPower;
+
+    [Header("세부 값")]
+    [SerializeField] float DashCoolTime;
     [SerializeField] float GroundHitDistance;
+
+    [Header("레이어")]
+    [SerializeField] LayerMask Ground;
 
     private Rigidbody2D rigid;
 
@@ -32,7 +40,7 @@ public class AgentMovement : MonoBehaviour
 
     public void Jump()
     {
-        RaycastHit2D groundHit = Physics2D.Raycast(transform.position, Vector2.down, GroundHitDistance);
+        RaycastHit2D groundHit = Physics2D.Raycast(transform.position, Vector2.down, GroundHitDistance, Ground);
         if(Input.GetKeyDown(KeyCode.Space) && groundHit)
         {
             rigid.velocity = new Vector2(rigid.velocity.x, PlayerJumpPower);
@@ -41,6 +49,9 @@ public class AgentMovement : MonoBehaviour
 
     public void Dash()
     {
-
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            rigid.velocity = new Vector2(DashPower, 0);
+        }
     }
 }
