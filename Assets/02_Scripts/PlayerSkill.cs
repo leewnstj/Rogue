9a96f5dd9d5493a2812dcefed3f355c489778ebf
leewnstj <ij35492581@gmail.com>
2023-02-05 20:20:51 +0900

    playerskill
    playerskill

---
+++ Assets/02_Scripts/PlayerSkill.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSkill : MonoBehaviour
+{
+    
+    private void SmokeSkill()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+
+        }
+    }
+}
