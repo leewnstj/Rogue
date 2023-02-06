ea79a426dba05caae259b4b9e6ce0b0c4fd93889
leewnstj <ij35492581@gmail.com>
2023-02-06 21:04:01 +0900

    attack

--- Assets/02_Scripts/PlayerAttack.cs
+++ Assets/02_Scripts/PlayerAttack.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
+    [SerializeField] Transform attackPoint;
+    [SerializeField] float AttackTime;
+    [SerializeField] float coolTIme;
     private float curTime;
 
-    public void Attack()
+    private void Update()
     {
-        
+        Vector2 len = Camera.main.ScreenToWorldPoint(Input.mousePosition) - attackPoint.position;
+        float z = Mathf.Atan2(len.y, len.x) * Mathf.Rad2Deg;
+        attackPoint.rotation = Quaternion.Euler(0, 0, z);
+
+        if(curTime <= 0)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("d");
+            }
+            curTime = AttackTime;
+        }
+        curTime -= Time.deltaTime;
     }
 }
