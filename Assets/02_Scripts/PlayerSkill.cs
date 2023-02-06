bf67dc2346b062411c038dd2a2590341dbbc3299
leewnstj <ij35492581@gmail.com>
2023-02-06 20:06:36 +0900

    playerAttack

--- Assets/02_Scripts/PlayerSkill.cs
+++ Assets/02_Scripts/PlayerSkill.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class PlayerSkill : MonoBehaviour
 {
-    
-    private void SmokeSkill()
+    [SerializeField] float UltimateCoolTime;
+    PlayerMovement pm;
+
+    public bool isUltimate;
+
+    private void Start()
+    {
+        pm = GetComponent<PlayerMovement>();
+    }
+    public void SmokeSkill()
     {
         if (Input.GetMouseButtonDown(1))
         {
@@ -13,11 +21,15 @@ public class PlayerSkill : MonoBehaviour
         }
     }
 
-    private void UltimateSkill()
+    public void UltimateSkill()
     {
         if (Input.GetKeyDown(KeyCode.R))
         {
-
+            isUltimate = true;
+        }
+        if (isUltimate)
+        {
+            pm.isDash = true;
         }
     }
 }
